35342742effa4a4145d7cbbe670132d2398db55f
PriyaTechmantra <priya.m@techmantra.co>
2024-08-27 18:03:16 +0530

    first commit

---
+++ app/Http/Controllers/Facility/OfficeController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Facility;
+
+use App\Models\Office;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Controllers\Controller;
+class OfficeController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function __construct()
+    {
+         $this->middleware('permission:view office', ['only' => ['index']]);
+         $this->middleware('permission:create office', ['only' => ['create','store']]);
+         $this->middleware('permission:update office', ['only' => ['update','edit']]);
+         $this->middleware('permission:delete office', ['only' => ['destroy']]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): View
+    {
+        $data = Office::latest()->paginate(5);
+        return view('facility.office.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(): View
+    {
+        return view('facility.office.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        request()->validate([
+            'name' => 'required',
+            'address' => 'required',
+        ]);
+    
+        Office::create($request->all());
+    
+        return redirect()->route('offices.index')
+                        ->with('success','Office created successfully.');
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Office $product): View
+    {
+        return view('facility.office.show',compact('product'));
+    }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id): View
+    {
+        $data = Office::find($id);
+        return view('facility.office.edit',compact('data'));
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Office $product): RedirectResponse
+    {
+         request()->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+    
+        $product->update($request->all());
+    
+        return redirect()->route('offices.index')
+                        ->with('success','Office updated successfully');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product): RedirectResponse
+    {
+        $product->delete();
+    
+        return redirect()->route('offices.index')
+                        ->with('success','Office deleted successfully');
+    }
+}
---
+++ database/seeders/UserRolePermissionSeeder.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserRolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Permissions
+        Permission::create(['name' => 'view role']);
+        Permission::create(['name' => 'create role']);
+        Permission::create(['name' => 'update role']);
+        Permission::create(['name' => 'delete role']);
+
+        Permission::create(['name' => 'view permission']);
+        Permission::create(['name' => 'create permission']);
+        Permission::create(['name' => 'update permission']);
+        Permission::create(['name' => 'delete permission']);
+
+        Permission::create(['name' => 'view user']);
+        Permission::create(['name' => 'create user']);
+        Permission::create(['name' => 'update user']);
+        Permission::create(['name' => 'delete user']);
+
+        Permission::create(['name' => 'view office']);
+        Permission::create(['name' => 'create office']);
+        Permission::create(['name' => 'update office']);
+        Permission::create(['name' => 'delete office']);
+        
+        Permission::create(['name' => 'view book']);
+        Permission::create(['name' => 'create book']);
+        Permission::create(['name' => 'update book']);
+        Permission::create(['name' => 'delete book']);
+
+
+        // Create Roles
+        $superAdminRole = Role::create(['name' => 'super-admin']); //as super-admin
+        $adminRole = Role::create(['name' => 'facility-admin']);
+        $lmsadminRole = Role::create(['name' => 'lms-admin']);
+        $userRole = Role::create(['name' => 'user']);
+
+        // Lets give all permission to super-admin role.
+        $allPermissionNames = Permission::pluck('name')->toArray();
+
+        $superAdminRole->givePermissionTo($allPermissionNames);
+
+        // Let's give few permissions to admin role.
+        //$adminRole->givePermissionTo(['create role', 'view role', 'update role']);
+       // $adminRole->givePermissionTo(['create permission', 'view permission']);
+        $lmsadminRole->givePermissionTo(['create book', 'view book', 'update book','delete book']);
+        $adminRole->givePermissionTo(['create office', 'view office', 'update office','delete office']);
+
+
+        // Let's Create User and assign Role to it.
+
+        $superAdminUser = User::firstOrCreate([
+                    'email' => 'superadmin@gmail.com',
+                ], [
+                    'name' => 'Super Admin',
+                    'email' => 'superadmin@gmail.com',
+                    'password' => Hash::make ('12345678'),
+                ]);
+
+        $superAdminUser->assignRole($superAdminRole);
+
+
+        $adminUser = User::firstOrCreate([
+                            'email' => 'facilityadmin@gmail.com'
+                        ], [
+                            'name' => 'Facility Admin',
+                            'email' => 'facilityadmin@gmail.com',
+                            'password' => Hash::make ('12345678'),
+                        ]);
+
+        $adminUser->assignRole($adminRole);
+
+
+        $lmsadminUser = User::firstOrCreate([
+                            'email' => 'lmsadmin@gmail.com',
+                        ], [
+                            'name' => 'Lms Admin',
+                            'email' => 'lmsadmin@gmail.com',
+                            'password' => Hash::make('12345678'),
+                        ]);
+
+        $lmsadminUser->assignRole($lmsadminRole);
+    }
+}
